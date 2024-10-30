92769d5708f4d209cbef9560580a7abe2ab819d3
Andrew Krivonos <119019613+breweryrecordingstudio@users.noreply.github.com>
2024-10-30 14:49:48 -0500

    update fixes

---
+++ migrations/2024_02_19_154120_create_plans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('heading')->nullable();
+            $table->string('per_hour')->nullable();
+            $table->string('half_day')->nullable();
+            $table->string('full_day')->nullable();
+            $table->longText('description')->nullable();
+            $table->longText('link')->nullable();
+            $table->integer('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+};
